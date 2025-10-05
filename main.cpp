ef9357f12caf41e2772bdd9417d56ed48cd373a1
so1218 <K024G0110@g.neec.ac.jp>
2025-10-05 15:06:30 +0900

    PG3_02_01完了

--- main.cpp
+++ main.cpp
@@ -1,43 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <stdio.h>
+#include <windows.h>
 
-using namespace std;
-
-int main() 
+template <typename T>
+T Min(T a, T b)
 {
-    // メールアドレスのリスト
-    vector<string> emails = 
-    {
-        "k024g1017@g.neec.ac.jp", "k024g0033@g.neec.ac.jp", "k024g0057@g.neec.ac.jp",
-        "k024g0020@g.neec.ac.jp", "k024g0109@g.neec.ac.jp", "k024g1031@g.neec.ac.jp",
-        "k024g0004@g.neec.ac.jp", "k024g0027@g.neec.ac.jp", "k024g0058@g.neec.ac.jp",
-        "k022g0113@g.neec.ac.jp", "k024g0007@g.neec.ac.jp", "k024g0083@g.neec.ac.jp",
-        "k024g0110@g.neec.ac.jp", "k024g0066@g.neec.ac.jp", "k023g0029@g.neec.ac.jp",
-        "k024g1030@g.neec.ac.jp", "k024g0106@g.neec.ac.jp", "k024g0089@g.neec.ac.jp",
-        "k024g0101@g.neec.ac.jp", "k024g0035@g.neec.ac.jp", "k024g1025@g.neec.ac.jp",
-        "k024g0059@g.neec.ac.jp", "k024g0006@g.neec.ac.jp", "k023g0122@g.neec.ac.jp",
-        "k024g0028@g.neec.ac.jp", "k024g1024@g.neec.ac.jp", "k024g0108@g.neec.ac.jp",
-        "k024g0061@g.neec.ac.jp", "k024g0104@g.neec.ac.jp", "k024g0038@g.neec.ac.jp",
-        "k024g0032@g.neec.ac.jp", "k024g0026@g.neec.ac.jp", "k024g0001@g.neec.ac.jp",
-        "k024g0009@g.neec.ac.jp", "k024g0112@g.neec.ac.jp", "k024g0011@g.neec.ac.jp",
-        "k024g0085@g.neec.ac.jp", "k024g0111@g.neec.ac.jp", "k024g0045@g.neec.ac.jp",
-        "k024g0103@g.neec.ac.jp", "k024g1002@g.neec.ac.jp", "k024g0078@g.neec.ac.jp",
-        "k024g0044@g.neec.ac.jp", "k024g0008@g.neec.ac.jp", "k024g0075@g.neec.ac.jp",
-        "k024g0091@g.neec.ac.jp", "k024g0064@g.neec.ac.jp", "k024g0051@g.neec.ac.jp",
-        "k024g0093@g.neec.ac.jp", "k024g0024@g.neec.ac.jp"
-    };
+    return (a < b) ? a : b;
+}
 
-    // 学籍番号順でソート
-    sort(emails.begin(), emails.end());
+int main()
+{
+    // int
+    int a = 1, b = 2;
+    int minInt = Min(a, b);
 
-    // 表示
-    cout << "学籍番号順" << endl;
-    for (const auto& email : emails)
-    {
-        cout << email << endl;
-    }
+    // float
+    float x = 2.1f, y = 2.0f;
+    float minFloat = Min(x, y);
+
+    // double
+    double p = 3.1, q = 3.0;
+    double minDouble = Min(p, q);
+
+    printf("MinInt: %d\n", minInt);
+    printf("MinFloat: %.2f\n", minFloat);
+    printf("MinDouble: %.2lf\n", minDouble);
 
     return 0;
 }
