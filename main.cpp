097efbf696d16fcc0a454717df6d5a25072c12d0
so1218 <K024G0110@g.neec.ac.jp>
2025-10-05 14:55:55 +0900

    PG3_01_02完了

--- main.cpp
+++ main.cpp
@@ -1,88 +1,43 @@
 #include <iostream>
-#include <list>
-#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-// 駅情報構造体
-struct Station 
-{
-    const char* name;
-    int openedYear;
-};
-
-// 駅を出力する関数
-void printStations(const list<Station>& stations, int year)
-{
-    cout << year << "年" << endl;
-    for (const auto& station : stations)
-    {
-        if (station.openedYear <= year)
-        {
-            cout << station.name << endl;
-        }
-    }
-    cout << endl;
-}
-
 int main() 
 {
-    list<Station> yamanotesen;
-
-    // 駅データの追加
-    yamanotesen.push_back({ "東京", 1970 });
-    yamanotesen.push_back({ "神田", 1970 });
-    yamanotesen.push_back({ "秋葉原", 1970 });
-    yamanotesen.push_back({ "御徒町", 1970 });
-    yamanotesen.push_back({ "上野", 1970 });
-    yamanotesen.push_back({ "鶯谷", 1970 });
-    yamanotesen.push_back({ "日暮里", 1970 });
-    yamanotesen.push_back({ "田端", 1970 });
-    yamanotesen.push_back({ "駒込", 1970 });
-    yamanotesen.push_back({ "巣鴨", 1970 });
-    yamanotesen.push_back({ "大塚", 1970 });
-    yamanotesen.push_back({ "池袋", 1970 });
-    yamanotesen.push_back({ "目白", 1970 });
-    yamanotesen.push_back({ "高田馬場", 1970 });
-    yamanotesen.push_back({ "新大久保", 1970 });
-    yamanotesen.push_back({ "新宿", 1970 });
-    yamanotesen.push_back({ "代々木", 1970 });
-    yamanotesen.push_back({ "原宿", 1970 });
-    yamanotesen.push_back({ "渋谷", 1970 });
-    yamanotesen.push_back({ "恵比寿", 1970 });
-    yamanotesen.push_back({ "目黒", 1970 });
-    yamanotesen.push_back({ "五反田", 1970 });
-    yamanotesen.push_back({ "大崎", 1970 });
-    yamanotesen.push_back({ "品川", 1970 });
-    yamanotesen.push_back({ "田町", 1970 });
-    yamanotesen.push_back({ "浜松町", 1970 });
-    yamanotesen.push_back({ "新橋", 1970 });
-    yamanotesen.push_back({ "有楽町", 1970 });
-
-    // 西日暮里
-    for (auto it = yamanotesen.begin(); it != yamanotesen.end(); ++it) 
+    // メールアドレスのリスト
+    vector<string> emails = 
     {
-        if (strcmp(it->name, "田端") == 0)
-        {
-            yamanotesen.insert(it, { "西日暮里", 2019 });
-            break;
-        }
-    }
+        "k024g1017@g.neec.ac.jp", "k024g0033@g.neec.ac.jp", "k024g0057@g.neec.ac.jp",
+        "k024g0020@g.neec.ac.jp", "k024g0109@g.neec.ac.jp", "k024g1031@g.neec.ac.jp",
+        "k024g0004@g.neec.ac.jp", "k024g0027@g.neec.ac.jp", "k024g0058@g.neec.ac.jp",
+        "k022g0113@g.neec.ac.jp", "k024g0007@g.neec.ac.jp", "k024g0083@g.neec.ac.jp",
+        "k024g0110@g.neec.ac.jp", "k024g0066@g.neec.ac.jp", "k023g0029@g.neec.ac.jp",
+        "k024g1030@g.neec.ac.jp", "k024g0106@g.neec.ac.jp", "k024g0089@g.neec.ac.jp",
+        "k024g0101@g.neec.ac.jp", "k024g0035@g.neec.ac.jp", "k024g1025@g.neec.ac.jp",
+        "k024g0059@g.neec.ac.jp", "k024g0006@g.neec.ac.jp", "k023g0122@g.neec.ac.jp",
+        "k024g0028@g.neec.ac.jp", "k024g1024@g.neec.ac.jp", "k024g0108@g.neec.ac.jp",
+        "k024g0061@g.neec.ac.jp", "k024g0104@g.neec.ac.jp", "k024g0038@g.neec.ac.jp",
+        "k024g0032@g.neec.ac.jp", "k024g0026@g.neec.ac.jp", "k024g0001@g.neec.ac.jp",
+        "k024g0009@g.neec.ac.jp", "k024g0112@g.neec.ac.jp", "k024g0011@g.neec.ac.jp",
+        "k024g0085@g.neec.ac.jp", "k024g0111@g.neec.ac.jp", "k024g0045@g.neec.ac.jp",
+        "k024g0103@g.neec.ac.jp", "k024g1002@g.neec.ac.jp", "k024g0078@g.neec.ac.jp",
+        "k024g0044@g.neec.ac.jp", "k024g0008@g.neec.ac.jp", "k024g0075@g.neec.ac.jp",
+        "k024g0091@g.neec.ac.jp", "k024g0064@g.neec.ac.jp", "k024g0051@g.neec.ac.jp",
+        "k024g0093@g.neec.ac.jp", "k024g0024@g.neec.ac.jp"
+    };
 
-    // 高輪ゲートウェイ
-    for (auto it = yamanotesen.begin(); it != yamanotesen.end(); ++it)
+    // 学籍番号順でソート
+    sort(emails.begin(), emails.end());
+
+    // 表示
+    cout << "学籍番号順" << endl;
+    for (const auto& email : emails)
     {
-        if (strcmp(it->name, "田町") == 0) 
-        {
-            yamanotesen.insert(it, { "高輪ゲートウェイ", 2022 });
-            break;
-        }
+        cout << email << endl;
     }
-
-    // 駅表示
-    printStations(yamanotesen, 1970);
-    printStations(yamanotesen, 2019);
-    printStations(yamanotesen, 2022);
 
     return 0;
 }
