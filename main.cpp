7617824b975599b867a257ff830fc409ca48fb1f
so1218 <K024G0110@g.neec.ac.jp>
2025-11-07 22:28:34 +0900

    PG02_03完了

--- main.cpp
+++ main.cpp
@@ -1,26 +1,45 @@
-#include <stdio.h>
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <time.h>   
 #include <windows.h>
 
-// 再帰的に時給を計算して、合計賃金を返す関数
-int recursiveWage(int hours, int wage)
+
+void ShowResult(int roll, int userGuess)
 {
-    if (hours == 0)
-        return 0;
-    return wage + recursiveWage(hours - 1, wage * 2 - 50);
+    printf("出目は %d でした。\n", roll);
+
+    int actualResult = roll % 2;
+
+    if (userGuess == actualResult)
+    {
+        printf("正解\n");
+    }
+    else
+    {
+        printf("不正解\n");
+    }
+}
+
+void DelayReveal(void (*fn)(int, int), unsigned int delayMs, int roll, int userGuess)
+{
+    Sleep(delayMs);
+
+    fn(roll, userGuess);
 }
 
 int main()
 {
-    int hours = 8;
+    int userGuess;
+    int roll;     
 
-    int general = hours * 1226;
+    srand((unsigned int)time(NULL));
 
-    int recursive = recursiveWage(hours, 100);
+    printf("半(奇数:1)か丁(偶数:0)か: ");
+    scanf_s("%d", &userGuess);
 
-    printf("働いた時間 %d時間\n", hours);
-    printf("一般的な賃金体系 %d円\n", general);
-    printf("再帰的な賃金体系 %d円\n", recursive);
+    roll = rand() % 6 + 1;
 
+    DelayReveal(ShowResult, 3000, roll, userGuess);
 
     return 0;
 }
