06be7a5be0ead4e2be1cd947721b8429115ee35e
so1218 <K024G0110@g.neec.ac.jp>
2025-12-26 13:37:14 +0900

    02-01完了

---
+++ 02-01/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <windows.h>
+
+template <typename T>
+T Min(T a, T b)
+{
+    return (a < b) ? a : b;
+}
+
+int main()
+{
+    int a = 1, b = 2;
+    int minInt = Min(a, b);
+
+    float x = 2.1f, y = 2.0f;
+    float minFloat = Min(x, y);
+
+    double p = 3.1, q = 3.0;
+    double minDouble = Min(p, q);
+
+    printf("MinInt: %d\n", minInt);
+    printf("MinFloat: %.2f\n", minFloat);
+    printf("MinDouble: %.2lf\n", minDouble);
+
+    return 0;
+}
