5addad36ad1f6e5457694f55d20b1627a5f622f5
so1218 <K024G0110@g.neec.ac.jp>
2025-12-26 13:38:23 +0900

    02-02完了

---
+++ 02-02/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <windows.h>
+
+int recursiveWage(int hours, int wage)
+{
+    if (hours == 0)
+        return 0;
+    return wage + recursiveWage(hours - 1, wage * 2 - 50);
+}
+
+int main()
+{
+    int hours = 8;
+
+    int general = hours * 1226;
+
+    int recursive = recursiveWage(hours, 100);
+
+    printf("働いた時間 %d時間\n", hours);
+    printf("一般的な賃金体系 %d円\n", general);
+    printf("再帰的な賃金体系 %d円\n", recursive);
+
+
+    return 0;
+}
